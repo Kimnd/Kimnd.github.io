dffba5ae177e456e3fdc71815d6182711457055c
Kimnd <fionagallag@gmail.com>
2021-03-12 20:30:11 -0800

    Fixed bug where fullname was never correctly assigned

--- js/kojimaJavascript.js
+++ js/kojimaJavascript.js
@@ -1,4 +1,16 @@
-:i_
-
 //Javascript code for the Kojima Name Generator!
 //
+var fullname = "brian david gilbert";
+function displayName() {
+  document.getElementById("displayFullName").innerHTML = fullname;
+  console.log("name to display: " + fullname + "\n");
+}
+function assignName() {
+  fullname = document.getElementById("fullname").value;
+  console.log("var fullname is: " + fullname + "\n");
+}
+function rollD(diceSides) {
+
+}
+
+//source: https://attacomsian.com/blog/javascript-generate-random-numbers
