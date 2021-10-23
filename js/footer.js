acbb8a0f5f62eac6824e7d74e20c443282889212
Frankie Gallagher <fionagallag@gmail.com>
2021-10-22 18:32:32 -0700

    Added an automatic footer generation similar to the menu one

---
+++ js/footer.js
@@ -0,0 +1,32 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	var linkedinnode = document.createElement('a');
+	var githubnode = document.createElement('a');
+	var emailnode = document.createElement('a');
+  var copyrightnode = document.createElement('p');
+	linkedinnode.href="https://www.linkedin.com/in/frankie-gallag-8374371a5";
+	githubnode.href="https://github.com/Kimnd";
+	emailnode.href="mailto:frankgallagherthe2nd@gmail.com";
+
+	var linkedintext = document.createTextNode("Linkedin");
+	var githubtext = document.createTextNode("Github");
+	var emailtext = document.createTextNode("Email");
+  var copyrighttext = document.createTextNode("© 2021 by Frankie Gallagher");
+
+	linkedinnode.appendChild(linkedintext);
+	githubnode.appendChild(githubtext);
+	emailnode.appendChild(emailtext);
+  copyrightnode.appendChild(copyrighttext);
+
+  document.getElementById("foot").appendChild(linkedinnode);
+  document.getElementById("foot").appendChild(githubnode);
+  document.getElementById("foot").appendChild(emailnode);
+  document.getElementById("foot").appendChild(copyrightnode);
+  // document.getElementById("foot").appendChild(faqnode);
+
+});
+        // <a href="https://www.linkedin.com/in/frankie-gallag-8374371a5">Linkedin</a>
+        // <a href="https://github.com/Kimnd">Github</a>
+        // <a href="mailto:frankgallagherthe2nd@gmail.com">Gmail</a>
+
