822c6def5e4a038d2c82476261977fa3051f69ba
Frankie Gallagher <fionagallag@gmail.com>
2021-11-12 00:15:23 -0800

    Made a small webpage to count the words and have them show up beside the writing area

---
+++ js/wordcounter.js
@@ -0,0 +1,56 @@
+//Script to count words in a box, mostly sourced from
+// https://codesource.io/building-a-word-counter-in-javascript/
+
+var count = document.getElementById("count");
+var input = document.getElementById('input');
+var currchar = document.getElementById('character');
+var charnum = document.getElementById('charnum');
+var str = "";
+//An event listener that notes when the user types
+/*
+var counttext = document.createTextNode("0");
+var countnode = document.getElementById("count");
+countnode.appendChild(counttext);
+*/
+input.addEventListener('keyup', function(e) {
+  // console.log("event listener fired!");
+  wordcount(e.target.value);
+
+});
+
+
+
+function wordcount(text) {
+  var text = input.value.split(' ');
+  var wordcount = 0;
+  for (var i = 0; i < text.length; i++) {
+    console.log("text being analyzed: [" + text[i] + "]");
+    if (text[i] !== ' ' && isword(text[i])) {
+      console.log("counted word");
+      wordcount++;
+    }
+  }
+  count.innerText = "the word count is: " + wordcount;
+  // console.log("word count is: " + wordcount);
+}
+function isword(str) {
+  var alphaNumericFound = false;
+  var i = 0;
+  while (i < str.length) {
+    var code = str.charCodeAt(i);
+    if ( /*(code <= 34 && code >= 33) ||*/
+         (code >= 97 && code <= 122) ||
+         (code >= 65 && code <= 90)){
+      alphaNumericFound = true;
+      console.log("alphanumeric true!");
+      currchar.style.color = "green";
+    } else {
+      currchar.style.color = "red";
+    }
+    currchar.innerText = str[i];
+    charnum.innerText = code;
+    console.log("char is: " + code);
+    i++;
+  }
+  return alphaNumericFound;
+}
