480944430ca22ec980aa242dc86d266c85e5d1bf
Frankie Gallagher <fionagallag@gmail.com>
2022-01-20 19:36:07 -0800

    Changed screen width code to something more readable

--- js/screen_width.js
+++ js/screen_width.js
@@ -6,15 +6,32 @@
 
 //var screenwidth = document.getElementByID("width");
 
+/*
 window.addEventListener("resize", function() {
-	//code sourced from https://www.geeksforgeeks.org/how-to-get-the-device-screhow-to-get-the-width-of-device-screen-in-javascript-en-width-in-javascript/
-	console.log("Width is: " + window.innerWidth + " pixels.");
-	/*
-	//for the css stuff: https://attacomsian.com/blog/javascript-set-css-styles
-	if (window.matchMedia("(min-width: 500px)").matches) {
-		console.log("Screen is at least 500px!");
-	} else {
-		console.log("TINY  SCREEN");
-	}
-	*/
+  //code sourced from https://www.geeksforgeeks.org/how-to-get-the-device-screhow-to-get-the-width-of-device-screen-in-javascript-en-width-in-javascript/
+//width = window.innerWidth;
+//if (width < 500) {
+//  document.getElementByID("width").style.fontSize = "60px"; doesn't work--nor does "style.size, style.font-size"
+//  window.getElementByID("width")
+//  window.getElementByClass("index_card").style.color = "blue";
+//}
+//console.log("Width is: " + window.innerWidth + " pixels.");
+
+  //for the css stuff: https://attacomsian.com/blog/javascript-set-css-styles
+  // if (window.matchMedia("(min-width: 500px)").matches) {
+  //   console.log("Screen is at least 500px!");
+  // } else {
+  //   console.log("TINY  SCREEN");
+  // }
+
 });
+*/
+window.addEventListener("resize", widthReport);
+
+//function that prints the screen width to the console
+function widthReport() {
+
+  width = window.innerWidth;
+  console.log("Width is: " + width + " pixels.");
+  return width;
+}
