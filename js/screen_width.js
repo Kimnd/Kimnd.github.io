8b29cacc6d870d5b17ac9a72b5f409f1798f8df3
Frankie Gallagher <fionagallag@gmail.com>
2022-01-25 13:58:33 -0800

    Tried and failed to add ability to show width of screen in the webpage itself

--- js/screen_width.js
+++ js/screen_width.js
@@ -26,14 +26,56 @@ window.addEventListener("resize", function() {
 
 });
 */
-window.addEventListener("resize", widthReport);
+//var showWidth = document.getElementByID("width_target");
+//window.addEventListener("resize", widthReport);
+
+var widthtarget = document.getElementById("widthtarget");
+  
+function getwidth() {
+  var width = window.innerWidth;
+  widthtarget.innerHTML = width + " pixels";
+}
+/*
+
+
+
+
+document.addEventListener("resize", widthReport);
 
 //function that prints the screen width to the console
 function widthReport() {
 
   width = window.innerWidth;
   console.log("Width is: " + width + " pixels.");
+//  document.getElementByID("width_target").innerHTML = "width is: " + width + " pixels.";
   return width;
 }
+/* Code to diplay width by innerHTML taken from geeks for geeks, tested with w3schools
+https://www.geeksforgeeks.org/how-to-get-the-device-screhow-to-get-the-width-of-device-screen-in-javascript-en-width-in-javascript/
+https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_onresize
 
-document.
+<!DOCTYPE html>
+<html> 
+  <head> 
+    <title> 
+      How to get the device screen width in JavaScript ?
+    </title>
+  </head> 
+    
+  <body onresize="getwidth()"> 
+    
+    <h1>GeeksForGeeks</h1>
+    <button onclick = "getwidth()">click here</button>
+    <p id = "widthtarget"></p>
+    <!-- Script to display the device screen width -->
+    <script>
+      var widthtarget = document.getElementById("widthtarget");
+        
+      function getwidth() {
+        var width = window.innerWidth;
+        widthtarget.innerHTML = width + " pixels";
+      }
+    </script> 
+  </body> 
+</html>
+*/
---
+++ js/test.js
@@ -0,0 +1,4 @@
+function largeFont() {
+  const dStyle = document.querySelector('style');
+  dStyle.innerHTML = 'p {font-size: 2rem;}';
+}
