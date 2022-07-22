c70d9f43a723d38b22d3e0cc21eefb2b89b4f794
Frankie Gallagher <fionagallag@gmail.com>
2022-07-21 22:21:28 -0700

    Removed content tag on html, that way the javascript can more easily ad the canvas object

--- js/snakecode.js
+++ js/snakecode.js
@@ -4,14 +4,14 @@ More specific sources will be provided as relevant in the code
 */
 //Initialization variables
 //Board variables
-var boardwidth;
-var boardheight;
+var boardwidth = 20;
+var boardheight = 20;
 
 document.addEventListener("DOMContentLoaded", function() {
 	// document.addEventListener("keyup", control);
 	console.log("Load event listener added");
-	init();
 	startGame();
+	init();
 })
 
 // console.log("In snakecode!");
@@ -20,26 +20,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function init(something) {
   console.log("inited!");
-  canvas = document.getElementById('gameCanvas');
-  ctx = canvas.getContext('2d');
+  // canvas = document.getElementByTagName("canvas");
+  // ctx = canvas.getContext("2d");
 }
 
 //Starts the game
 function startGame() {
 	console.log("Game has started!");
-	// gameBoard.start();
+	gameBoard.start();
 }
-/*
+///*
 var gameBoard = {
 	canvas : document.createElement("canvas"),
-	console.log("Canvas created");
+	// console.log("Canvas created");
 	start : function() {
-		// this.canvas.width = boardwith;
-		// this.canvas.height = boardheight;
-		// this.context = this.canvas.getContext("2d");
-	}
+		this.canvas.width = boardwidth;
+		this.canvas.height = boardheight;
+		this.context = this.canvas.getContext("2d");
+		// target = document.getElementByID("gameBoard");
+    document.body.replaceChild(this.canvas, document.body.childNodes[4]);
+    // document.getElementById("gameBoard").insertBefore(this.canvas, document.body.childNodes[0]);
+    },
 	clear : function() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	}
-}
-*/
+};
+// */
