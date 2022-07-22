1a545c99c0f408e55c38baaa0a5f4b5095c08691
Frankie Gallagher <fionagallag@gmail.com>
2022-07-21 20:56:24 -0700

    Successfully linked js file so it can be called from the html page

--- js/snakecode.js
+++ js/snakecode.js
@@ -3,8 +3,43 @@ https://www.w3schools.com/graphics/game_intro.asp
 More specific sources will be provided as relevant in the code
 */
 //Initialization variables
+//Board variables
+var boardwidth;
+var boardheight;
+
+document.addEventListener("DOMContentLoaded", function() {
+	// document.addEventListener("keyup", control);
+	console.log("Load event listener added");
+	init();
+	startGame();
+})
+
+// console.log("In snakecode!");
+
+//let something = 1;
+
+function init(something) {
+  console.log("inited!");
+  canvas = document.getElementById('gameCanvas');
+  ctx = canvas.getContext('2d');
+}
 
 //Starts the game
 function startGame() {
 	console.log("Game has started!");
+	// gameBoard.start();
 }
+/*
+var gameBoard = {
+	canvas : document.createElement("canvas"),
+	console.log("Canvas created");
+	start : function() {
+		// this.canvas.width = boardwith;
+		// this.canvas.height = boardheight;
+		// this.context = this.canvas.getContext("2d");
+	}
+	clear : function() {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+}
+*/
